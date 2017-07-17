fe56ea4c67a2185985a67f47c1606a8ba0ab0018
jessysc <jessy.sc@outlook.com>
2017-07-16 22:54:24 -0300

    Update Aluno.java

--- src/sistemaEscolar/Model/Aluno.java
+++ src/sistemaEscolar/Model/Aluno.java
@@ -1,6 +1,7 @@
 package sistemaEscolar.Model;
 
 import java.time.LocalDate;
+import sistemaEscolar.Control.ControlAluno;
 
 /**
  * Classe aluno que extende de Usuário 
@@ -8,34 +9,78 @@ import java.time.LocalDate;
  */
 public class Aluno extends Usuario 
 {
-    private final int nota;
+    private double nota1, nota2, nota3, notaFinal;
+    
 
-    public Aluno(String nome, LocalDate dataNascimento, String email, String usuario, String senha,int nota)
+    public Aluno(String nome, LocalDate dataNascimento, String email, String usuario, String senha,double nota1, double nota2, double nota3, double notaFinal)
     {
         super(nome,dataNascimento,email,usuario,senha);
-        this.nota = nota;
+        this.nota1 = 0;
+        this.nota2 = 0;
+        this.nota3 = 0;        
+        this.notaFinal = 0;  
+        
     }
  
-    public int getNota()
+    public double getNotaFinal()
     {
-        return nota;
+        return notaFinal;
     }
     
+    public double getN1()
+    {
+        return nota1;
+    }
+    
+    public double getN2()
+    {
+        return nota2;
+    }
+    
+    public double getPrj()
+    {
+        return nota3;
+    }
+    
+    public void setN1(double nota){
+        this.nota1 = nota;
+    }
+    
+     public void setN2(double nota){
+        this.nota2 = nota;
+    }
+     
+      public void setN3(double nota){
+        this.nota3 = nota;
+    }
+      
+     public void setNotaFinal(double nota){
+        this.notaFinal = nota;
+    }  
+    
+        
     @Override
     public String toString()
     {
-        return super.toString()+" |Nota: "+nota;
+        return super.toString()+" |Nota: "+notaFinal;
+        
     }
 
     /**
      * Sobrescrita do método compareTo da interface Comparable,
      * definindo que os alunos serão comparados pela pontuação.
+     * @param controlAluno
      * @param aluno
      * @return 
      */
     
-    public int compareTo(Aluno aluno)
+    public boolean notas(ControlAluno controlAluno, Aluno aluno)
     {
-        return aluno.getNota() - this.nota;
+       return controlAluno.update(aluno);
+    }
+    
+    public double compareTo(Aluno aluno)
+    {
+        return aluno.getNotaFinal() - this.notaFinal;
     }
 }
