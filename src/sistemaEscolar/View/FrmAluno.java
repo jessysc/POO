8491245e8c4e705f5a233b0ae116a29db1582f4b
jessysc <jessy.sc@outlook.com>
2017-07-16 22:55:37 -0300

    Update FrmAluno.java

--- src/sistemaEscolar/View/FrmAluno.java
+++ src/sistemaEscolar/View/FrmAluno.java
@@ -19,19 +19,22 @@ public class FrmAluno extends javax.swing.JFrame
         this.logged = logged;
         this.currentAluno = (Aluno)logged.getLoggedUser();
         txtName.setText(currentAluno.getNome());
-        txtNota.setText(String.valueOf(currentAluno.getNota()));
+        txtNota.setText(String.valueOf(currentAluno.getNotaFinal()));
+        txtN1.setText(String.valueOf(currentAluno.getN1()));
+        txtN2.setText(String.valueOf(currentAluno.getN2()));
+        txtN3.setText(String.valueOf(currentAluno.getPrj()));
     }   
     
     public void load_janela()
     {
-        txtNota.setText(String.valueOf(currentAluno.getNota()));
+        txtNota.setText(String.valueOf(currentAluno.getNotaFinal()));
         txtName.setText(currentAluno.getNome());
     }
     
     
         
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
@@ -39,16 +42,21 @@ public class FrmAluno extends javax.swing.JFrame
         jPanel1 = new javax.swing.JPanel();
         txtNota = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        txtN2 = new javax.swing.JTextField();
+        txtN1 = new javax.swing.JTextField();
+        txtN3 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        btnNotas = new javax.swing.JMenuItem();
         btnMyAccount = new javax.swing.JMenuItem();
         btnLogoff = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel1.setText("Seja bem vindo!");
+        jLabel1.setText("Notas");
 
         txtName.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         txtName.setText("Nome ");
@@ -60,7 +68,7 @@ public class FrmAluno extends javax.swing.JFrame
         txtNota.setText("00");
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel4.setText("Nota");
+        jLabel4.setText("Nota Final");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -68,31 +76,44 @@ public class FrmAluno extends javax.swing.JFrame
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(21, 21, 21)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel4)
-                    .addComponent(txtNota))
-                .addContainerGap(22, Short.MAX_VALUE))
+                .addComponent(jLabel4)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(txtNota)
+                .addGap(31, 31, 31))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel4)
-                .addGap(2, 2, 2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtNota)
-                .addGap(24, 24, 24))
+                .addGap(20, 20, 20))
         );
 
-        jMenu1.setText("Ações");
-
-        btnNotas.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.SHIFT_MASK));
-        btnNotas.setText("Notas");
-        btnNotas.addActionListener(new java.awt.event.ActionListener() {
+        txtN2.setEditable(false);
+        txtN2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnNotasActionPerformed(evt);
+                txtN2ActionPerformed(evt);
             }
         });
-        jMenu1.add(btnNotas);
+
+        txtN1.setEditable(false);
+
+        txtN3.setEditable(false);
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel2.setText("Prova 1:");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel3.setText("Prova 2:");
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel5.setText("Projeto:");
+
+        jMenu1.setText("Ações");
 
         btnMyAccount.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.SHIFT_MASK));
         btnMyAccount.setText("Sua Conta");
@@ -126,62 +147,91 @@ public class FrmAluno extends javax.swing.JFrame
                         .addContainerGap()
                         .addComponent(txtName))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(132, 132, 132)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel2))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(43, 43, 43)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel5)
+                                    .addComponent(jLabel3))))
+                        .addGap(36, 36, 36)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtN3, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtN1, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtN2, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(175, 175, 175)
                         .addComponent(jLabel1)))
-                .addContainerGap(140, Short.MAX_VALUE))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(156, 156, 156)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(159, Short.MAX_VALUE)))
+                .addContainerGap(192, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(61, 61, 61))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 214, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 65, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtN1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addGap(21, 21, 21)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtN2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtN3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel5))
+                        .addGap(73, 73, 73))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)))
                 .addComponent(txtName)
                 .addContainerGap())
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(91, 91, 91)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(91, Short.MAX_VALUE)))
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
 
-    private void btnLogoffActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnLogoffActionPerformed
-    {//GEN-HEADEREND:event_btnLogoffActionPerformed
+    private void btnLogoffActionPerformed(java.awt.event.ActionEvent evt)                                          
+    {                                              
         logged.logoff();
         this.dispose();
         FrmLogin login = new FrmLogin(logged, 3);
         login.setVisible(true);
-    }//GEN-LAST:event_btnLogoffActionPerformed
+    }                                         
 
-    private void btnMyAccountActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnMyAccountActionPerformed
-    {//GEN-HEADEREND:event_btnMyAccountActionPerformed
+    private void btnMyAccountActionPerformed(java.awt.event.ActionEvent evt)                                             
+    {                                                 
         FrmUserAccount<Aluno> fca = new FrmUserAccount(currentAluno,this,3);
         fca.setVisible(true);
-    }//GEN-LAST:event_btnMyAccountActionPerformed
+    }                                            
 
-    private void btnNotasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNotasActionPerformed
-        FrmNotas frmNotas = new FrmNotas();
-        frmNotas.setVisible(true);
-    }//GEN-LAST:event_btnNotasActionPerformed
+    private void txtN2ActionPerformed(java.awt.event.ActionEvent evt) {                                      
+        // TODO add your handling code here:
+    }                                     
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private javax.swing.JMenuItem btnLogoff;
     private javax.swing.JMenuItem btnMyAccount;
-    private javax.swing.JMenuItem btnNotas;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtN1;
+    private javax.swing.JTextField txtN2;
+    private javax.swing.JTextField txtN3;
     private javax.swing.JLabel txtName;
     private javax.swing.JLabel txtNota;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
